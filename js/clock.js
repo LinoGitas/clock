f50e264e53145cf9a61198f9ccbfb430254b1186
LinoGitas <63246130+LinoGitas@users.noreply.github.com>
2020-05-05 11:05:06 +0300

    AM PM Clock

--- js/clock.js
+++ js/clock.js
@@ -7,22 +7,38 @@ let HTML = '';
 for ( let h=0; h<24; h++){
     let hours = h;
 
-    if (hours <10 )
+    if (hours < 10 )
     {
         hours = '0' + hours;
     }
 
-    HTML += `<div class="time">${hours}:00:00</div>`;
+    //HTML += `<div class="time">${hours}:00:00</div>`;
+
+    let am = 'AM';
+    let AmPm = h%12;
+
+    if (AmPm === 0 )
+    {
+        AmPm = '12';      
+    }
+
+    if (hours >= 12 )
+    {
+        am = 'PM';      
+    }
+    
+    HTML += `<div class="time">${hours}:00 = ${AmPm}:00 ${am}</div>`;
 
     for ( let m=0; m<60; m++){
         let minutes = m;
 
-        if (minutes <10 )
+        if (minutes < 10 )
         {
             minutes = '0' + minutes;
-        }
+        }      
 
         //HTML += `<div class="time">${hours}:${minutes}:00</div>`;
+        //HTML += `<div class="time">${hours}:${minutes}:00 = ${AmPm}:${minutes}:00 ${am}</div>`;
         /*
         for ( let s=0; s<60; s++){
             let seconds = s;
